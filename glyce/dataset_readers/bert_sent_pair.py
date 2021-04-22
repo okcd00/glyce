c8972eaf24e82d0450d00a6af104de4de37ab3eb
Chen Dian <okcd00@users.noreply.github.com>
2021-04-22 17:52:55 +0800

    Update bert_sent_pair.py
    Fix: load bq dataset correctly.

--- glyce/dataset_readers/bert_sent_pair.py
+++ glyce/dataset_readers/bert_sent_pair.py
@@ -129,18 +129,18 @@ class BQProcessor(DataProcessor):
     def get_train_examples(self, data_dir):
         """See base class."""
         return self._create_examples(
-            self._read_tsv(os.path.join(data_dir, "train.json")), "train")
+            self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
 
     def get_dev_examples(self, data_dir):
         """See base class."""
         return self._create_examples(
-            self._read_tsv(os.path.join(data_dir, "valid.json")),
+            self._read_tsv(os.path.join(data_dir, "valid.tsv")),
             "dev_matched")
 
     def get_test_examples(self, data_dir):
         """See base class."""
         return self._create_examples(
-            self._read_tsv(os.path.join(data_dir, "test.json")),
+            self._read_tsv(os.path.join(data_dir, "test.tsv")),
             "dev_matched")
 
     def get_labels(self):
@@ -151,7 +151,6 @@ class BQProcessor(DataProcessor):
         """Creates examples for the training and dev sets."""
         examples = []
         for (i, line) in enumerate(lines):
-            line = json.loads(line[0])
             if i < 2:
                print("-"*10) 
                print("check  loading example") 
@@ -160,7 +159,7 @@ class BQProcessor(DataProcessor):
             guid = "%s-%s" % (set_type, i)
             text_a = line[1] # .replace(" ", "")
             text_b = line[2] # .replace(" ", "")
-            label = line
+            label = line[0]
             examples.append(
                 InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
         return examples
